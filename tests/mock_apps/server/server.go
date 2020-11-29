9bc341c4e7c19de8d1f5d9a5817ce8eff140f1a7
Mateusz Kyc <mateusz.kyc@gmail.com>
2020-11-29 20:49:53 +0100

    remove boring server function

--- tests/mock_apps/server/server.go
+++ tests/mock_apps/server/server.go
@@ -6,16 +6,12 @@ import (
 	"net/http"
 )
 
-func handleRequests() {
-	http.HandleFunc("/", root)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
 func root(w http.ResponseWriter, _ *http.Request) {
 	_, _ = fmt.Fprint(w, "root endpoint")
 	log.Println("hit: root")
 }
 
 func main() {
-	handleRequests()
+	http.HandleFunc("/", root)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
